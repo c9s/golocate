b6e54d9384282998b95b71f4ced52eaf50c55f4a
c9s <cornelius.howl@gmail.com>
2013-02-02 11:24:48 +0800

    remove concatFileItems func

--- fileitem.go
+++ fileitem.go
@@ -13,13 +13,6 @@ func (p * FileItem) String() string {
   return p.Path
 }
 
-func ConcatFileItems(old1, old2 []FileItem) []FileItem {
-   newslice := make([]FileItem, len(old1) + len(old2))
-   copy(newslice, old1)
-   copy(newslice[len(old1):], old2)
-   return newslice
-}
-
 func PrettySize(bytes int) string {
   if bytes < 1024 {
     return strconv.Itoa(bytes) + "B"
